3067fcedfeb02e3bdb067c53c3c56cc7f9ee403b
Steffen <steffen.albrecht@desy.de>
2020-11-18 18:00:51 +0100

    adds forgotten renaming in Manager.py (el7_worker -> sl6_container)

--- Manager.py
+++ Manager.py
@@ -166,7 +166,7 @@ class JobManager(object):
                             exit(-1)
                     ask = False
                 if batchstatus != 1:
-                    process.pids[it-1] = resubmit(self.outputstream+process.name,process.name+'_'+str(it),self.workdir,self.header,self.el7_worker)
+                    process.pids[it-1] = resubmit(self.outputstream+process.name,process.name+'_'+str(it),self.workdir,self.header,self.sl6_container)
                     #print 'Resubmitted job',process.name,it, 'pid', process.pids[it-1]
                     self.printString.append('Resubmitted job '+process.name+' '+str(it)+' pid '+str(process.pids[it-1]))
                     if process.status != 0: process.status =0
